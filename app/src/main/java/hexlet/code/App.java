8aef4f8c1b21ede5647a5b214529b55dd2852c82
Bazarkin Dmitrii <bazarkin.dmitrii@gmail.com>
2022-10-29 15:04:43 +0700

    refactoring project structure, create new class Calculator.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,8 @@
 package hexlet.code;
 
+import hexlet.code.games.Calculator;
+import hexlet.code.games.Even;
+
 import java.util.Scanner;
 
 public class App {
@@ -9,6 +12,7 @@ public class App {
                 Please enter the game number and press Enter.
                 1 - Greet
                 2 - Even
+                3 - Calc
                 0 - Exit
                 Your choice:\s""");
         String choice = scanner.next();
@@ -16,6 +20,7 @@ public class App {
         switch (choice) {
             case "1" -> Cli.greet();
             case "2" -> Even.startGame();
+            case "3" -> Calculator.startCalc();
             case "0" -> System.out.println("Come back if you want to play!");
             default -> System.out.println("Incorrect input!");
         }
---
+++ app/src/main/java/hexlet/code/games/Calculator.java
@@ -0,0 +1,9 @@
+package hexlet.code.games;
+
+import hexlet.code.Cli;
+
+public class Calculator {
+    public static void startCalc() {
+        Cli.greet();
+    }
+}
