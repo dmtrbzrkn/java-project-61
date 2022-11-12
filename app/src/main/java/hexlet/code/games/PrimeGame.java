e8322ad439ee3b22cceb14330e9ff7951a318f6e
Bazarkin Dmitrii <bazarkin.dmitrii@gmail.com>
2022-11-12 15:40:31 +0700

    Making edits

--- app/src/main/java/hexlet/code/games/PrimeGame.java
+++ app/src/main/java/hexlet/code/games/PrimeGame.java
@@ -21,15 +21,14 @@ public class PrimeGame {
     }
 
     private static boolean isPrime(int number) {
-        if (number > 1) {
-            for (int i = 2; i < number; i++) {
-                if (number % i == 0) {
-                    return false;
-                }
-            }
-            return true;
-        } else {
+        if (number <= 1) {
             return false;
         }
+        for (int i = 2; i < Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
