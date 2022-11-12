e376cec0bf914a93fe09c37001099e94181380a0
Bazarkin Dmitrii <bazarkin.dmitrii@gmail.com>
2022-11-12 16:40:16 +0700

    Refactoring

--- app/src/main/java/hexlet/code/games/EvenGame.java
+++ app/src/main/java/hexlet/code/games/EvenGame.java
@@ -1,26 +1,14 @@
 package hexlet.code.games;
 
-import static hexlet.code.Engine.NUMBERS_OF_ROUND;
-import static hexlet.code.Engine.FROM_VALUE_NUMBER;
-import static hexlet.code.Engine.TO_VALUE_NUMBER;
-import static hexlet.code.utils.RandomUtils.generateNumber;
+import hexlet.code.utils.PrimeOrEven;
+
 import static hexlet.code.Engine.runGame;
 
 public class EvenGame {
     public static final String TASK = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+    public static final String NAME_OF_GAME = "EvenGame";
 
     public static void startEvenGame() {
-        String[][] answersAndQuestions = new String[NUMBERS_OF_ROUND][2];
-
-        for (int i = 0; i < NUMBERS_OF_ROUND; i++) {
-            int currentNumber = generateNumber(FROM_VALUE_NUMBER, TO_VALUE_NUMBER);
-            answersAndQuestions[i][0] = String.valueOf(currentNumber);
-            answersAndQuestions[i][1] = isEven(currentNumber) ? "yes" : "no";
-        }
-        runGame(TASK, answersAndQuestions);
-    }
-
-    private static boolean isEven(int number) {
-        return number % 2 == 0;
+        runGame(TASK, PrimeOrEven.runOneOfGame(NAME_OF_GAME));
     }
 }
